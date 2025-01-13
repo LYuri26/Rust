014cff938c721de7e48caf1e77df2b8ce58cbc0a
Lenon Yuri <lenonyurisilva26@gmail.com>
2025-01-13 13:45:07 -0300

    13/01/25

---
+++ target/debug/deps/rustaulas-3b2b4b35b4551c4d.d
@@ -0,0 +1,9 @@
+C:\Users\lyuri\OneDrive - Sistema FIEMG\Documentos\GitHub\Rust\target\debug\deps\librustaulas-3b2b4b35b4551c4d.rmeta: main.rs Cargo.toml
+
+C:\Users\lyuri\OneDrive - Sistema FIEMG\Documentos\GitHub\Rust\target\debug\deps\rustaulas-3b2b4b35b4551c4d.d: main.rs Cargo.toml
+
+main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
---
+++ target/debug/deps/rustaulas-f6706a58b3ee9c66.d
@@ -0,0 +1,9 @@
+C:\Users\lyuri\OneDrive - Sistema FIEMG\Documentos\GitHub\Rust\target\debug\deps\librustaulas-f6706a58b3ee9c66.rmeta: main.rs Cargo.toml
+
+C:\Users\lyuri\OneDrive - Sistema FIEMG\Documentos\GitHub\Rust\target\debug\deps\rustaulas-f6706a58b3ee9c66.d: main.rs Cargo.toml
+
+main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
